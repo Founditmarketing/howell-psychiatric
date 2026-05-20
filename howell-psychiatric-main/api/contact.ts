aafcb0c2b99317a56da514357264ccd47abaae33
J-Albright <jason@founditmarketing.com>
2026-05-20 04:17:22 -0500

    Update TO_ADDRESS to info@howell-psychiatric-mental-wellness-llc.com

--- howell-psychiatric-main/api/contact.ts
+++ howell-psychiatric-main/api/contact.ts
@@ -4,7 +4,7 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const FROM_ADDRESS = 'info@howell-psychiatric-mental-wellness-llc.com';
-const TO_ADDRESS   = 'jason@founditmarketing.com';
+const TO_ADDRESS   = 'info@howell-psychiatric-mental-wellness-llc.com';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Only accept POST
